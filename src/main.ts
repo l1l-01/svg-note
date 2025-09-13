5f4bb8d5963ede752fa464ebe0a2cefd5a1f0919
l1l <khalil23ar@gmail.com>
2025-09-13 23:44:27 +0100

    Setting up EJS template engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as methodOverride from 'method-override';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // parse HTML form bodies
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // allow HTML forms to use PUT/DELETE via _method
+  app.use(methodOverride('_method'));
+
+  // static assets (css/js/images)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // EJS views
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
